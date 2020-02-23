3c7c693d5251ecc7c39c058f6a86353a11812799
JohnyBGoode123 <johnybgoode123@gmail.com>
2020-02-23 16:54:19 +0300

    FirstCommitDB

--- app/src/main/java/com/example/diethelperapp/MainActivity.kt
+++ app/src/main/java/com/example/diethelperapp/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
-        // Не уверен что здесь все сработает как надо, возможно нужно каким то образом проверять флаг isLoading, и если он true
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
